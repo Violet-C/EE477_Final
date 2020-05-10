b54f43bb578fbd4718640cbf9750ba7d4b60d7a8
Violet Cunningham <Violet.Cunningham@stmartin.edu>
2020-05-09 17:22:46 -0700

    added comments to action files

--- violet_actions/src/violet_action_client.py
+++ violet_actions/src/violet_action_client.py
@@ -1,18 +1,18 @@
 #! /usr/bin/env python 
-import rospy
+import rospy          # python client library for ROS
 import time           # python timing (not normal for ROS)
 import actionlib      # for actions
 from violet_actions.msg import \
-	TimerAction, TimerGoal, TimerResult, TimerFeedback
+	TimerAction, TimerGoal, TimerResult, TimerFeedback   # message types needed
 
-def the_feedback_cb(feedback):           # callback function
+def the_feedback_cb(feedback):                 # callback function
 	print('[Feedback] Time elapsed %f' % 
-		(feedback.time_elapsed.to_sec()))
+		(feedback.time_elapsed.to_sec()))      # print time elapsed (in seconds)
 	print('[Feedback] Time remaining: %f' % 
-		(feedback.time_remaining.to_sec()))
+		(feedback.time_remaining.to_sec()))    # print time remaining (in seconds)
 
-rospy.init_node('timer_action_client')   # initialize client node
-client = actionlib.SimpleActionClient(   # register client
+rospy.init_node('timer_action_client')         # initialize client node
+client = actionlib.SimpleActionClient(         # register client
 	'timer',        # server name
 	TimerAction     # action message type
 )
@@ -33,13 +33,13 @@ client.send_goal(goal, feedback_cb=the_feedback_cb)   # send goal
 client.wait_for_result()          # wait for action server to finish
 
 # print results:
-print('[Result] State: %d' % (client.get_state()))
-print('[Result] Status: %s' % (client.get_goal_status_text()))
-if client.get_result():
+print('[Result] State: %d' % (client.get_state()))              # number of final state
+print('[Result] Status: %s' % (client.get_goal_status_text()))  # final status (success, failure, etc.)
+if client.get_result():                                 # if results exist
 	print('[Result] Time elapsed: %f' % 
-		(client.get_result().time_elapsed.to_sec()))
+		(client.get_result().time_elapsed.to_sec()))    # print time elapsed (in seconds)
 	print('[Result] Updates sent: %d' % 
-		(client.get_result().updates_sent))
+		(client.get_result().updates_sent))             # print number of updates sent
 
 
 
--- violet_actions/src/violet_action_server.py
+++ violet_actions/src/violet_action_server.py
@@ -1,42 +1,45 @@
 #! /usr/bin/env python 
-import rospy
+import rospy          # python client library for ROS
 import time           # python timing (not normal for ROS)
 import actionlib      # for actions
 from violet_actions.msg import \
-	TimerAction, TimerGoal, TimerResult, TimerFeedback
+	TimerAction, TimerGoal, TimerResult, TimerFeedback   # message types needed
 
-def do_timer(goal):   # action function
-	start_time = time.time()
-	update_count = 0
+def do_timer(goal):            # action function
+	start_time = time.time()   # get time at start
+	update_count = 0           # to count number of updates sent
 
-	if goal.time_to_wait.to_sec() > 60.0:      # stops after 60 seconds 
-		result = TimerResult()
-		result.time_elapsed = rospy.Duration.from_sec(time.time() - start_time)
-		result.updates_sent = update_count
-		server.set_aborted(result, "Aborted - too long to wait!")
+	if goal.time_to_wait.to_sec() > 60.0:      # stop if input time is oover 60 seconds
+		result = TimerResult()                                     # create result object
+		result.time_elapsed = rospy.Duration.from_sec(
+			time.time() - start_time)                              # calculate time elapsed
+		result.updates_sent = update_count                         # number of updates sent
+		server.set_aborted(result, "Aborted - too long to wait!")  # error message
 		return
 
 	while (time.time() - start_time) < goal.time_to_wait.to_sec():
-		# goes until time to wait is over
+		# go until time to wait is over
 
 		if   server.is_preempt_requested():    # stops if preempted by user
-			result = TimerResult()
+			result = TimerResult()                                 # create result object
 			result.time_elapsed = rospy.Duration.from_sec(
-				time.time() - start_time)
-			result.updates_sent = update_count
-			server.set_preempted(result, "Timer preempted")
+				time.time() - start_time)                          # calculate time elapsed
+			result.updates_sent = update_count                     # number of updates sent
+			server.set_preempted(result, "Timer preempted")        # error message
 			return
 
-		feedback = TimerFeedback()
-		feedback.time_elapsed = rospy.Duration.from_sec(time.time()- start_time)
-		feedback.time_remaining = goal.time_to_wait - feedback.time_elapsed
-		server.publish_feedback(feedback)
-		update_count += 1
-		time.sleep(1.0)
-	result = TimerResult()
-	result.time_elapsed = rospy.Duration.from_sec(time.time() - start_time)
-	result.updates_sent = update_count
-	server.set_succeeded(result, "Timer completed successfully")
+		feedback = TimerFeedback()             # create feedback object
+		feedback.time_elapsed = rospy.Duration.from_sec(
+			time.time()- start_time)           # calculate time elapsed (in seconds)
+		feedback.time_remaining = goal.time_to_wait - feedback.time_elapsed # calculate time remaining (in seconds)
+		server.publish_feedback(feedback)      # publish feedback
+		update_count += 1                      # add one to count of updates sent
+		time.sleep(1.0)                        # wait for 1 second
+	result = TimerResult()                     # create result object
+	result.time_elapsed = rospy.Duration.from_sec(
+		time.time() - start_time)              # calculate time elapsed (in seconds)
+	result.updates_sent = update_count         # final number of updates sent
+	server.set_succeeded(result, "Timer completed successfully")   # success message
 
 rospy.init_node('timer_action_server')         # initialize server node
 server = actionlib.SimpleActionServer(         # register server
@@ -46,5 +49,5 @@ server = actionlib.SimpleActionServer(         # register server
 	False                   # always set to False
 	)                  
 
-server.start()
-rospy.spin()
+server.start()              # start action
+rospy.spin()                # wait for requests
