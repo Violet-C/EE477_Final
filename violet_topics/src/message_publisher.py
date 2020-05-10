5aaadfc74d8c21bee1182fcbe7a5473df6419f6b
Violet Cunningham <Violet.Cunningham@stmartin.edu>
2020-05-10 09:59:58 -0700

    added comments to message files

--- violet_topics/src/message_publisher.py
+++ violet_topics/src/message_publisher.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python
-import rospy
-from violet_topics.msg import Complex  # Custom message type
+#!/usr/bin/env python    
+import rospy                           # python client library for ROS
+from violet_topics.msg import Complex  # custom message type
 from random import random              # to generate rand. #s
 
 rospy.init_node('message_publisher')   # initialize node
 
 pub = rospy.Publisher(                 # register topic
-	'complex',                          # name topic
-	Complex,                            # type of message
-	queue_size=3
+	'complex',                         # name topic
+	Complex,                           # type of message
+	queue_size=3                       # set queue size (similar to buffer)
 )
 
 rate = rospy.Rate(2)                   # set rate to 2 Hz
 
-while not rospy.is_shutdown():
+while not rospy.is_shutdown():         # loop until stopped by user
 	msg = Complex()                    # declare message type
-	msg.real = random()                # assign values
-	msg.imaginary = random()
+	msg.real = random()                # assign random value
+	msg.imaginary = random()           # assign random value
 
 	pub.publish(msg)                   # publish
 	rate.sleep()                       # pause to keep rate
--- violet_topics/src/message_subscriber.py
+++ violet_topics/src/message_subscriber.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
-import rospy
-from violet_topics.msg import Complex
+#!/usr/bin/env python 
+import rospy                             # python client library for ROS
+from violet_topics.msg import Complex    # custom message type
 
-def callback(msg):
-	print 'Real:', msg.real
-	print 'Imaginary:', msg.imaginary
+def callback(msg):                       # callback function
+	print 'Real:', msg.real              # print real part of complex number
+	print 'Imaginary:', msg.imaginary    # print imaginary part of complex number
 	print                                # blank line
 
 rospy.init_node('message_subscriber')    # initialize node
